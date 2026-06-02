b294a9d277f011b76aa1e075028bff33711ade1f
david <davidodile01@gmail.com>
2026-06-03 00:25:37 +0100

    feat: Add Expense serializer

--- app/serializers.py
+++ app/serializers.py
@@ -1,1 +1,13 @@
 from rest_framework import serializers
+from .models import Expense, Income, Budget
+
+
+class ExpenseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Expense
+        fields = [
+            "title",
+            "amount",
+            "category",
+            "date",
+        ]
