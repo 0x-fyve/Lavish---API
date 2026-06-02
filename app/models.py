444c9979f8a27142409bf47d1ca0eb91752170b4
david <davidodile01@gmail.com>
2026-06-03 00:11:00 +0100

    feat(models): Add Expense model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+User = get_user_model()
+
+class Expense(models.Model):
+    user = models.ForeignKey(on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=50)
+    date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Income(models.Model):
