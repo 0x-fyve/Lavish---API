e13ed9463fec658bffad9fddd615404f63f902ea
david <davidodile01@gmail.com>
2026-06-03 00:12:01 +0100

    feat: Add Expense model

--- app/models.py
+++ app/models.py
@@ -13,3 +13,8 @@ class Expense(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
 class Income(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
